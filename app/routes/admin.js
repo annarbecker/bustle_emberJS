bfd1c6264338bb189cc1cabbcb84840b3a7ac270
Anna Becker and Brad London <anna.r.becker@gmail.com>
2016-03-29 14:01:46 -0700

    Finish update and delete functionality on admin side.

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -9,6 +9,19 @@ export default Ember.Route.extend({
       var newArticle = this.store.createRecord('article', params);
       newArticle.save();
       this.transitionTo('index');
+    },
+    update(article, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          article.set(key,params[key]);
+        }
+      });
+      article.save();
+      this.transitionTo('admin');
+    },
+    destroyArticle(article) {
+      article.destroyRecord();
+      this.transitionTo('admin');
     }
-  }, 
+  }
 });
